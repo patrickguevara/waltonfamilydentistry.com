aa7bd54d101f5a0591c2befbd8c3d4846ac82e73
Patrick Guevara <patricksamuelguevara@gmail.com>
2025-11-17 14:47:39 -0600

    feat: add Filament resource for contact submissions

--- app/Filament/Resources/ContactSubmissionResource.php
+++ app/Filament/Resources/ContactSubmissionResource.php
@@ -3,12 +3,15 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ContactSubmissionResource\Pages;
+use App\Filament\Resources\ContactSubmissionResource\RelationManagers;
 use App\Models\ContactSubmission;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ContactSubmissionResource extends Resource
 {
@@ -131,6 +134,13 @@ class ContactSubmissionResource extends Resource
                 ]),
             ])
             ->defaultSort('created_at', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
     }
 
     public static function getPages(): array
